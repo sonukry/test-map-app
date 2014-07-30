3bba9247a13f2182aa71a63cca4120f368563536
Shubham Verma <shubham.verman@gmail.com>
2014-07-30 23:09:02 +0530

    Added support to fetch and display geofences in front-end

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,6 +1,7 @@
-;(function(angular, _) {
+;
+(function (angular, _) {
   'use strict';
-  
+
   var app = angular.module('myApp', ['google-maps', 'ngResource']);
 
   // route config
@@ -19,34 +20,62 @@
   // factory for Places
   app.factory('Places', function ($http) {
     var url = '/api/places';
-    
+
     return {
-      getNearby: function(id) {
+      getNearby: function (id) {
         return $http.get('/api/nearby/' + id);
       },
       get: function () {
         return $http.get(url);
       },
-      post: function(data) {
+      post: function (data) {
         return $http.post(url, data);
       },
-      del: function(id) {
+      del: function (id) {
         return $http.delete(url + '/' + id);
       }
     };
   });
-  
-  app.controller('MainCtrl', function($scope, Places) {
+
+  // factory for Geofences
+  app.factory('Geofences', function ($http) {
+    var url = '/api/geofences';
+
+    return {
+      get: function () {
+        return $http.get(url);
+      }
+    };
+  });
+
+  app.controller('MainCtrl', function ($scope, Places, Geofences) {
     Places
       .get()
-      .success(function(docs) {
+      .success(function (docs) {
         $scope.markers = docs;
       });
+
+    Geofences
+      .get()
+      .success(function (docs) {
+        $scope.geofences = docs;
+      });
   });
-  
+
   // map controller
   app.controller('MapCtrl', function ($scope, Places) {
-    
+
+    $scope.poly = {
+      stroke: {
+        color: '#6060FB',
+        weight: 3
+      },
+      fill: {
+        color: 'skyblue',
+        opacity: 0.8
+      }
+    };
+
     $scope.map = {
       center: {
         latitude: 13,
@@ -54,14 +83,14 @@
       },
       zoom: 9,
       events: {
-        click: function(map, evt, args) {
+        click: function (map, evt, args) {
           var latLng = args[0].latLng;
-      
+
           var lat = latLng.lat();
           var lng = latLng.lng();
-          
-          var payload = { 
-            name: prompt('Enter place name'), 
+
+          var payload = {
+            name: prompt('Enter place name'),
             loc: {
               type: 'Point',
               coordinates: [lng, lat]
@@ -69,51 +98,53 @@
           };
 
           Places
-          .post({data: payload})
-          .success(function(doc) {
-            console.log('POST SUC:', doc);
-            $scope.$parent.markers.push(doc);
-          })
-          .error(function() {
-            console.log('POST ERR:', payload);
-          });
+            .post({
+              data: payload
+            })
+            .success(function (doc) {
+              console.log('POST SUC:', doc);
+              $scope.$parent.markers.push(doc);
+            })
+            .error(function () {
+              console.log('POST ERR:', payload);
+            });
         }
       }
     };
   });
 
   // markerList controller 
-  app.controller('MarkerListCtrl', function($scope, Places) {
-  
+  app.controller('MarkerListCtrl', function ($scope, Places) {
+
     // to remove a marker
-    $scope.removeMarker = function(evt) {
+    $scope.removeMarker = function (evt) {
       var id = evt.target.attributes.id.value;
-      
+
       Places
         .del(id)
-        .success(function() {
+        .success(function () {
           console.log('DEL SUC:', id);
-          
-          $scope.$parent.markers = _.reject($scope.$parent.markers, function(m) {
+
+          $scope.$parent.markers = _.reject($scope.$parent.markers, function (m) {
             return m._id == id;
           });
         })
-        .error(function() {
+        .error(function () {
           console.log('DEL ERR:', id);
         });
     };
-    
+
     // to get nearby place
-    $scope.getNearby = function(evt) {
+    $scope.getNearby = function (evt) {
       var id = evt.target.attributes.id.value;
-      
+
       Places
         .getNearby(id)
-        .success(function(data) {
+        .success(function (data) {
           alert(data.name + ' at ' + Math.round(data.dis * 100) / 100 + ' km');
         });
     };
-    
-  });  
-  
+
+  });
+
 }(window.angular, window._));
